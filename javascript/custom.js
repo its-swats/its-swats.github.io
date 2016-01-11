18c5869e482db646fd8e70f97e2c87f078335433
Shawn Watson <shawnw86@gmail.com>
2016-01-10 16:33:54 -0800

    Add handlebars and projects handling

---
+++ javascript/custom.js
@@ -0,0 +1,41 @@
+$(function(){
+  projects(); 
+  addContent();
+})
+
+
+var projects = function(){
+  $('#projects').on('click', function(event){
+    event.preventDefault();
+    $('#emptySpace').animate({'height': '7.5em'})
+    openProjectsTab();
+  })
+}
+
+var addContent = function(){
+  $('#emptySpace').delegate('.icon', 'click', function(event){
+    
+  })
+}
+
+var openProjectsTab = function(){
+  var template = $('#projectDetails').html();
+  var template = Handlebars.compile(template);
+  var context = {'project': projectData};
+  var template = template(context);
+  $('#emptySpace').html(template);
+}
+
+var projectData = [
+                    {label: 'hsCardLookup', image: 'hearthstone'},
+                    {label: 'TasteMaker', image: 'tastemaker'},
+                    {label: 'timeTrackr', image: 'timetrackr', cssOptions: 'iconify'}
+                  ]
+
+
+
+
+
+
+
+
