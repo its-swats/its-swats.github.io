c1ace6a8346fb86b3b267f0cb5a326f1fc9344cd
Shawn Watson <shawnw86@gmail.com>
2016-06-14 22:36:12 -0700

    Update Burger cooker for multiple burgers per grill

--- BobsBurgers/application.js
+++ BobsBurgers/application.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
 function prepareObjects() {
 	// On launch, initialize the grill and the order list
 	list = new OrderList
-	grills = new Grill
-	list.startCooking();
-	grills.watchForBurgers();
-	prepareGrillPanel();
+	list.startOrders();
+	grillMaster = new GrillMaster
+	grillMaster.startCooking();
+
 };
 
 function OrderList() {
 	this.orders = []
-	this.startCooking = function(){
+	this.startOrders = function(){
 		setInterval(function(){
 			// Create an order every 5 seconds
 			this.orders.push(new Order)
@@ -38,14 +38,33 @@ var Order = (function() {
 	}
 })();
 
-function Grill(grill, burger) {
-	this.grills = []
+function GrillMaster(grill, burger) {
+	this.grills = [new Grill(1), new Grill(2), new Grill(3), new Grill(4), new Grill(5)]
+	this.addToGrill = function(grill, burger){
+		this.grills[grill-1].addToGrill(list.orders[burger-1]);
+	};
+	this.startCooking = function(){
+		for (var i = 0; i < this.grills.length; i++) {
+			this.grills[i].prepareGrill();
+			this.grills[i].watchForBurgers();
+		};
+	};
+};
+
+function Grill(id){
+	this.id = id
+	this.burgers = []
 	this.cookingTimer = 5
 	this.clearGrill = function(){
 		// Resets the grill to default, both on screen and in the object itself
-		this.grills = []
+		this.burgers = []
 		this.cookingTimer = 5
-		$('.grill .individual-order').remove();
+		$("#grill-"+this.id+" .individual-order").remove();
+		this.prepareGrill();
+	};
+	this.addToGrill = function(burger){
+		// Adds a passed in burger to the grill
+		this.burgers.push(burger)
 	};
 	this.watchForBurgers = function(){
 		// Check to see if at least 3 burgers are on the grill
@@ -56,13 +75,9 @@ function Grill(grill, burger) {
 			this.cookingTimer == 0 ? this.finishBurgers() : null
 		}.bind(this), 1000)
 	};
-	this.addToGrill = function(burger){
-		// Adds a passed in burger to the grill
-		this.grills.push(list.orders[burger-1])
-	};
 	this.canItCook = function(){
 		// Return true or false, depending on length of grill array
-		return this.grills.length >= 3
+		return this.burgers.length >= 3
 	};
 	this.cookBurger = function(){
 		this.cookingTimer --
@@ -70,15 +85,39 @@ function Grill(grill, burger) {
 	this.finishBurgers = function() {
 		// Create template for Completed Orders div
 		// Returns the grill to its initial state, and unlocks the grill slots
-		var type = this.grills[0].type
-		var orderNumber = this.grills.map(function(num){return num.id})
+		var type = this.burgers[0].type
+		var orderNumber = this.burgers.map(function(num){return num.id})
 		var source = $('#complete-order')
 		var context = {type: type, orderNumber: orderNumber}
 		handleTemplate(source, context, $('#complete-orders'), false)
 		this.clearGrill();
-		prepareGrillPanel();
 	};
-};
+	// Set the Grill divs as 'droppable'
+	// Allow them to accept the order divs
+	// Highlight when the correct orders are dragged
+	this.prepareGrill = function(){
+		$('#grill-'+this.id).droppable({
+			accept: ".individual-order",
+			activeClass: "highlight",
+			disabled: false,
+
+			drop: function(event, ui){
+				// Return the order to the order list if it does not dock on the grill
+				$(ui.draggable).detach().css({top:0, left:0}).appendTo(this);
+				// Prevents items from being dragged off the grill once placed
+				// Also prevents additional items from going on to the same grill
+				$(ui.draggable).draggable({disabled: true})
+				// Changes the classes that the Grill allows 
+				// This prevents a veggie burger from going on to a meat grill, for example
+				$('#'+this.id).droppable("option", "accept", "." + $(ui.draggable).attr('id').match(/\w+/)[0])
+				// Add the order object to the grill object for further calculations
+				grillId = $(this).attr('id').match(/\d+/)[0]
+				burgerId = $(ui.draggable).attr('id').match(/\d+/)[0]
+				grillMaster.addToGrill(grillId, burgerId)
+			}
+		});		
+	};
+}
 
 function handleTemplate(template, context, location, draggable){
 	// Javascript templating function
@@ -98,34 +137,4 @@ function handleTemplate(template, context, location, draggable){
 		});
 	};
 };
-
-
-function prepareGrillPanel() {
-	// Set the Grill divs as 'droppable'
-	// Allow them to accept the order divs
-	// Highlight when the correct orders are dragged
-	$('.grill').droppable({
-		accept: ".individual-order",
-		activeClass: "highlight",
-		disabled: false,
-
-		drop: function(event, ui){
-			// Return the order to the order list if it does not dock on the grill
-			$(ui.draggable).detach().css({top:0, left:0}).appendTo(this);
-			// Prevents items from being dragged off the grill once placed
-			// Also prevents additional items from going on to the same grill
-			$(ui.draggable).draggable({disabled: true})
-			$(this).droppable({
-				disabled: true
-			})
-			// Changes the classes that the Grill allows 
-			// This prevents a veggie burger from going on to a meat grill, for example
-			$('.grill').droppable("option", "accept", "." + $(ui.draggable).attr('id').match(/\w+/)[0])
-			// Add the order object to the grill object for further calculations
-			burgerId = $(ui.draggable).attr('id').match(/\d+/)[0]
-			grills.addToGrill(burgerId)
-		},
-	});
-};
-
 
